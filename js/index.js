7f4ed71a5817ff98cc45643167074ee9d99bd5ab
Edwar Frank Pons Bessio <epons@qsocialnow.com>
2023-01-26 14:10:57 -0300

    Ejercicio 17: Eventos

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// const btnAlert = document.querySelector("#btn-alert");
+
+// btnAlert.addEventListener("click", () => {
+//  alert("Click en el Botón");
+// });
+
+$("#btn-alert").click(() => {
+ console.log("Hola, estoy utilizando JQuery");
+});
